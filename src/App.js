ee4c4ebed813163f061e634bb4920283e5d60bb1
Alaina Musselwhite <alainam0111@gmail.com>
2024-07-08 18:54:51 -0400

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 //U67724813
 //Alaina Musselwhite 
 
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import './App.css';
 import Gallery from './Gallery';
 import Header from './Header';
 
@@ -16,20 +15,8 @@ const teamMembers = [
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="Meet the Team" />
+      <Gallery members={teamMembers} />
     </div>
   );
 }
